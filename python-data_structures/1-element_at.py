34d78f543a565c0e0f028c83b9b462e1a492c6ad
ChernetAsmamaw <c.asmamaw@alustudent.com>
2022-11-03 03:04:07 -0700

    fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,9 @@
 # If the value is negative or out of the list's range it returnes None
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-    else:
+    if idx < 0 or idx > (len(my_list) - 1)
+    
+     return None
+    
+else:
         return my_list[idx]
