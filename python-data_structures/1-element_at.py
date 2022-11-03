b569fabb8a96e81cd9f898f9d18f4ffb07144afa
ChernetAsmamaw <c.asmamaw@alustudent.com>
2022-11-03 04:25:04 -0700

    Write a function that retrieves an element from a list like in C

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # The function retrieves an element from the list 
 # If the idx is negative or out of range it returnes None
-
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list) -1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     else:
         return my_list[idx]
