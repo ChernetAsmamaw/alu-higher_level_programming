f66647fc59e02e80f55081db9c5ef35eb6567875
ChernetAsmamaw <c.asmamaw@alustudent.com>
2022-11-03 11:54:03 -0700

    Write a function that adds 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Returns a tuple with 2 integers
-# The 1st element is an addition of each of the 1st arguments & same for the 2nd
-#Tuple smaller than2 use 0 and for tupler bigger than 2 use first 2 intigers
+# The 1st element is an addition of each of the 1st arguments & same for 2nd
+# Tuple smaller than2 use 0 and for tupler bigger than 2 use first 2 intigers
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
