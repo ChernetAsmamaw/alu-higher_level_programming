9b0cd0e814ddebbc10e029932af9d3fef1379847
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-10 18:25:28 +0200

    Function that prints the first x elements of a list and only integers

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    answer = 0
+ddef safe_print_division(a, b):
     try:
-        answer = a / b
-    except (ZeroDivisionError, ValueError, TypeError):
-        answer = None
+        result = a / b
+        return result
+    except (ZeroDivisionError, TypeError):
+        result = None
+        return result
     finally:
-        print("Inside result: {}".format(answer))
-    return
+        print("Inside result: {}".format(result))
