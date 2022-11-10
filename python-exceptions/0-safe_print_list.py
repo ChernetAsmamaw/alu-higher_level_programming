ed1126d700495e9f436ad785df8d2049ee28dbd2
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-10 18:06:29 +0200

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(add), end="")
             i += 1
-          except IndexError:
-              print()
-     print()
-     return i
+        except IndexError:
+            print()
+    print()
+    return i
