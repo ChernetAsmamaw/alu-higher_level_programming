d8f4b4b242338f5ab2c08fd8e918751fc6c04bc8
ChernetAsmamaw <c.asmamaw@alustudent.com>
2022-11-17 02:35:40 -0800

    def

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Returns the list of available attributes and methods of an objects
+''' Returns the list of available attributes and methods of an objects '''
 
 
 def lookup(obj):
+    '''define the function'''
     return (dir(obj))
