451a1e9cb93eacd2ab379a13eeb1d9f6de226d5d
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-17 19:34:29 +0200

    Same class or inherit from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-''' returns True if the object is an instance of a class that inherited (directly or indirectly) 
-from the specified class ; otherwise False '''
+''' Same class or inherit from '''
 
 
 def inherits_from(obj, a_class):
