bcda7cd7b6abb683c9b62797561207c92741f42c
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-17 16:55:47 +0200

    Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False '''
+
+
+def inherits_from(obj, a_class):
+    ''' checks if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class '''
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
