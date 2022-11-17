a348aed7211ffa98695b1ea6b0cfecbbc8c2a7a6
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-17 19:29:37 +0200

    Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' returns True if the object is an instance of, instance of a class that inherited from '''
+''' Same class or inherit from '''
 
 
 def is_kind_of_class(obj, a_class):
