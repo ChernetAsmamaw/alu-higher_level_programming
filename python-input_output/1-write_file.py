ffbcded89a04cc6c321138b069468485c56b4a87
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-18 12:51:02 +0200

    Log parsing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-number_of_lines = __import__('1-number_of_lines').number_of_lines
+"""
+In this module a single function that writes a string is created.
+"""
 
-filename = "my_file_0.txt"
-nb_lines = number_of_lines(filename)
-print("{} has {:d} lines".format(filename, nb_lines))
+
+def write_file(filename="", text=""):
+    """ Writes a string to a text file (UTF8) and returns the
+    number of characters written:
+    Args:
+        filename (str): String with the name of the file.
+        text (str): String to write in the file.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
