bd0bb5134329ae303da98d35e6b4c44d4fa9f6d2
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-18 12:19:51 +0200

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+In this module a single function that creates a class and has
+method to print dict represenattion.
+"""
+
+
+class Student:
+    """ Class that represents students.
+    Attributes:
+        first_name (str): First name of the student
+        last_name (str): Last name of the student
+        age (int): Age of the student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Initializes the class. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns dictionary of the class attributes. """
+        if attrs is None:
+            return self.__dict__
+
+        is_list_string = True
+        for elem in attrs:
+            if not isinstance(elem, str):
+                is_list_string = False
+                break
+
+        answer = {}
+        if is_list_string:
+            for i in attrs:
+                if i in list(self.__dict__.keys()):
+                    answer[i] = self.__dict__[i]
+                else:
+                    continue
+            return answer
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the students instance. """
+        for key in list(json.keys()):
+            setattr(self, key, json[key])
