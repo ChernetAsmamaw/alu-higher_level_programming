d5b23884bd182a1843de372fc91d9c8ae70c42db
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-18 17:32:11 +0200

    load save add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""add_item
-"""
-
+""" add_item """
 
 import sys
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
