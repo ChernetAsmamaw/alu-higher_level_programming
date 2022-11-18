2e83bc9560a03075e15ac1c201d2ac27c6397d93
ChernetAsmamaw <c.asmamaw@alustudnet.com>
2022-11-18 17:02:51 +0000

    log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' After every ten lines or the input of a keyboard interruption (CTRL + C),
+'''After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.'''
