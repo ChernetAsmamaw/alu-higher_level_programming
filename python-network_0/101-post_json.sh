43c999999d8838ce0cd48b4173573ae21f75f6e6
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-18 21:44:58 +0200

    Update 101-post_json.sh

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display methods
-curl -s -X POST $1 -d @"$2" --header "Content-Type: application/json"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
