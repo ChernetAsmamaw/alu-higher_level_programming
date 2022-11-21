f01462a27ad98121ae863ba4c4dd416a456149e4
ChernetAsmamaw <c.asmamaw@alustudnet.com>
2022-11-21 10:06:21 +0000

    show status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alu-intranet.hbtn.io/status """
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
-    with request.urlopen(https://alu-intranet.hbtn.io/status) as request:
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as request:
+        information = request.read()
+        print("Body response:")
+        print("\t- type:", type(information))
+        print("\t- content:", information)
+        print("\t- utf8 content:", str(information)[2:-1])
