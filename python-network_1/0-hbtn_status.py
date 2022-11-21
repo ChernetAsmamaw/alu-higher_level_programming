ba22c7cf4395b564b2541505b5636805477d157b
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-21 13:07:31 +0200

    show status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+"""Python script that fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     import urllib.request as request
-    with request.urlopen('https://alu-intranet.hbtn.io/status') as page:
+    with request.urlopen('https://intranet.hbtn.io/status') as page:
         html = page.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format((html))
-        print("\t- utf8 content: {}".format(html))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
