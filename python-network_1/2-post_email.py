b92eec19e0fd2b99501ea75d4fb8353621062d96
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-23 12:46:01 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,11 +7,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    value = {'email': email}
-    data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        answer = response.read()
-        print(str(answer)[2:-1])
+        print(response.read().decode("utf-8"))
