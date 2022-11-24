fb9b998eff6aa2df1a8616d0724516dd7d363987
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-24 16:03:48 +0200

    create htbn_0c_0 database

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,2 @@
 --  create the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE [IF NOT EXISTS] htbn_0c_0;
-SHOW CREATE DATABASE htbn_0c_0;
-USE htbn_0c_0;
+CREATE DATABASE IF NOT EXISTS 'htbn_0c_0';
