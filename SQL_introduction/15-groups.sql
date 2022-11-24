b0ca39e19d1946585e3781b00199072003ff864f
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-24 18:33:05 +0200

    group same scores

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+--  list the number of records with the same score in the table second_table of the database hbtn_0c_0.
+--  list should be sorted by the number of records (descending)
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
