a8872b74fa9ae84051e59641b8b8b8aac665bd81
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-25 22:59:41 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
