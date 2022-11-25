3a09ecab31505ffeb1297b8053468cc2dc19bf2f
ChernetAsmamaw <c.asmamaw@alustudent.com>
2022-11-25 20:05:30 +0000

    fix

--- python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This module contains a script to fetch a URL """
-import requests
-import sys
-
-
-if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    print(r.headers.get('X-Request-Id'))
