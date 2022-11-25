bf0ff9dfc00856f813f97bf1423466944d7c9d3b
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-25 19:18:41 +0200

    create  database and table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Create the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa.states' (
+       'id' INT PRIMARY KEY AUTO_INCREMENT,
+       'name' VARCHAR(256) NOT NULL);
