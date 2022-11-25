439d1b337cb95a93b7a892e551fff12d8f4ac293
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-11-25 18:59:57 +0200

    create table force_name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
--- create the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null (
-id INT DEFAULT 1, name VARCHAR(256));
+-- creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+name VARCHAR(256) NOT NULL);
