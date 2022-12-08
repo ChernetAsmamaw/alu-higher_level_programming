c0d349bb5b4b1925a31b54d0d19aa1dc09c1c806
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-12-08 17:11:30 +0200

    tdd matrix multiplication

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -37,7 +37,7 @@ def matrix_mul(m_a, m_b):
             if l3 == 0:
                 raise ValueError("m_b can't be empty")
         if l3 != len(item):
-            raise TypeError("each row of m_a must be of the same size")
+            raise TypeError("each row of m_b must be of the same size")
         for j in item:
             if type(j) is not int and type(j) is not float:
                 raise TypeError("m_b should contain only integers or floats")
