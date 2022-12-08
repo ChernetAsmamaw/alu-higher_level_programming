68089170d2466cfff3dc239621d34bbce92b7437
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-12-08 11:56:03 +0200

    tdd say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,32 +1,14 @@
->>> say_my_name = __import__('3-say_my_name').say_my_name
+#!/usr/bin/python3
+'''
+This is the "3-say_my-name" module.
+The module supplies one function, say_my_name.
+'''
 
 
-#TypeError
-
-    ### first_name and last_name must be strings
-
-    >>> say_my_name(True)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-    >>> say_my_name("Max", 2795)
-    Traceback (most recent call last):
-    TypeError: last_name must be a string
-
-    >>> say_my_name(None)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-    >>> say_my_name()
-    Traceback (most recent call last):
-    TypeError: say_my_name() missing 1 required positional argument: 'first_name'
-
-
-
-#General Case
-
-    >>> say_my_name("Max")
-    My name is Max 
-
-    >>> say_my_name("Holberton", "School")
-    My name is Holberton School
+def say_my_name(first_name, last_name=""):
+    '''prints My name is <first name> <last name>'''
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
