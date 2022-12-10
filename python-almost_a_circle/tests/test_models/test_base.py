95abc10bab468efd628c7727c7edb364619ec19a
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2022-12-10 14:38:16 +0200

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,97 +1,75 @@
 #!/usr/bin/python3
-""" Module for test Base class """
+"""Test case"""
+
+import os
 import unittest
+
 from models.base import Base
-from models.square import Square
 from models.rectangle import Rectangle
-from io import StringIO
-# for unittest 
-from unittest import TestCase
-from unittest.mock import patch
+from models.square import Square
 
 
-class TestBaseMethods(unittest.TestCase):
-    """ Suite to test Base class """
+class TestBase(unittest.TestCase):
+    """Test class"""
 
-    def setUp(self):
-        """ Method invoked for each test """
+    def test_basic(self):
+        """Test basic func"""
+        base = Base()
+        base_1 = Base()
+        base_89 = Base(89)
+        self.assertEqual(base.id, 1)
+        self.assertEqual(base_1.id, 2)
+        self.assertEqual(base_89.id, 89)
+
+    def test_to_json_string(self):
+        """Test Func"""
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+        self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
+
+    def test_from_json_string(self):
+        """Test Func"""
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+        self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
+
+    def test_save_to_file(self):
+        """Test func"""
         Base._Base__nb_objects = 0
 
-    def test_id(self):
-        """ Test assigned id """
-        new = Base(1)
-        self.assertEqual(new.id, 1)
-
-    def test_id_default(self):
-        """ Test default id """
-        new = Base()
-        self.assertEqual(new.id, 1)
-
-    def test_id_nb_objects(self):
-        """ Test nb object attribute """
-        new = Base()
-        new2 = Base()
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 2)
-        self.assertEqual(new3.id, 3)
-
-    def test_id_mix(self):
-        """ Test nb object attributes and assigned id """
-        new = Base()
-        new2 = Base(1024)
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 1024)
-        self.assertEqual(new3.id, 2)
-
-    def test_string_id(self):
-        """ Test string id """
-        new = Base('1')
-        self.assertEqual(new.id, '1')
-
-    def test_more_args_id(self):
-        """ Test passing more args to init method """
-        with self.assertRaises(TypeError):
-            new = Base(1, 1)
-
-    def test_access_private_attrs(self):
-        """ Test accessing to private attributes """
-        new = Base()
-        with self.assertRaises(AttributeError):
-            new.__nb_objects
-
-    def test_save_to_file_1(self):
-        """ Test JSON file """
         Square.save_to_file(None)
-        res = "[]\n"
-        with open("Square.json", "r") as file:
-            with patch('sys.stdout', new=StringIO()) as str_out:
-                print(file.read())
-                self.assertEqual(str_out.getvalue(), res)
 
-        try:
-            os.remove("Square.json")
-        except:
-            pass
+        self.assertTrue(os.path.isfile("Square.json"))
+
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(), '[]')
 
         Square.save_to_file([])
-        with open("Square.json", "r") as file:
-            self.assertEqual(file.read(), "[]")
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+            self.assertEqual(type(fname.read()), str)
 
-    def test_save_to_file_2(self):
-        """ Test for JSON file """
+        Square.save_to_file([Square(1)])
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(),
+                             '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+        Base._Base__nb_objects = 0
+
         Rectangle.save_to_file(None)
-        res = "[]\n"
-        with open("Rectangle.json", "r") as file:
-            with patch('sys.stdout', new=StringIO()) as str_out:
-                print(file.read())
-                self.assertEqual(str_out.getvalue(), res)
-        try:
-            os.remove("Rectangle.json")
-        except:
-            pass
+        self.assertTrue(os.path.isfile("Rectangle.json"))
+        
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(), '[]')
 
         Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), "[]")
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+            self.assertEqual(type(fname.read()), str)
+
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(),
+                             '[{"id": 1, "width": 1, '
+                             '"height": 2, "x": 0, "y": 0}]')
