3739f61d248ff172aacaacd61b00543b1785dc2c
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2023-02-09 15:34:03 +0300

    Update 101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,31 +1,25 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-const episodeId = String(process.argv[2]);
-const requestURL = 'https://swapi-api.hbtn.io/api/films/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/' + episodeId + '/';
-
-request(requestURL, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const jsonBodyFilms = JSON.parse(body).results;
-    const numberFilms = jsonBodyFilms.length;
-    for (let filmIdx = 0; filmIdx < numberFilms; filmIdx++) {
-      if (String(jsonBodyFilms[filmIdx].url) === filmURL) {
-        const lsCharacters = jsonBodyFilms[filmIdx].characters;
-        const numCharacters = lsCharacters.length;
-        for (let index = 0; index < numCharacters; index++) {
-          request(lsCharacters[index], function (error, response, body) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log(JSON.parse(body).name);
-            }
-          });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const characters = JSON.parse(body).characters;
+    characters_dict = {};
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) console.log(error);
+        else {
+          let id = JSON.parse(body).url;
+          id = id.replace('https://swapi-api.hbtn.io/api/people/', '');
+          id = id.replace('/', '');
+          const name = JSON.parse(body).name;
+          characters_dict[id] = (name);
         }
-        break;
-      }
+      });
+    });
+    for (let id in characters_dict) {
+        console.log(characters_dict[id]);
     }
   }
 });
