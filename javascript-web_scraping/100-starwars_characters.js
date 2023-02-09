90a24d97a6adbe6e878b82a8fcbc659db50d7db5
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2023-02-09 15:33:35 +0300

    Update 100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-const episodeId = String(process.argv[2]);
-const requestURL = 'https://swapi-api.hbtn.io/api/films/' + episodeId + '/';
-
-request(requestURL, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const jsonChar = JSON.parse(body).characters;
-    const numberChar = jsonChar.length;
-    for (let index = 0; index < numberChar; index++) {
-      request(jsonChar[index], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) console.log(error);
+        else console.log(JSON.parse(body).name);
       });
-    }
+    });
   }
 });
