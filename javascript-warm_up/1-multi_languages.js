401c8c1b35c51aee6f17e5acbdd8ed20487d4f8d
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2023-02-10 11:28:34 +0300

    javaScript warm up

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
-console.log(myVar);
-myVar = 'Python is cool';
-console.log(myVar);
-myVar = 'JavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
