c1b53d8009e4a72a33e3804abd5925df3dcb2618
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2023-02-12 14:25:57 +0300

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const myString = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myString);
